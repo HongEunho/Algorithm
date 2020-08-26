a0284500c890ee30e4810d8a1af07d4e28a97285
HongEunho <46186664+HongEunho@users.noreply.github.com>
2020-08-26 19:37:37 +0900

    MachineGame(Programmers) Stack
    MachineGame(Programmers) Stack

---
+++ MachineGame.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution{
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i : moves)
+		{
+			for(int j=0; j<board.length;j++)
+			{
+				if(board[j][i-1] != 0 )
+				{
+					
+				}
+			}
+		}
+		
+		
+		
+		return answer;
+	}
+}
+public class MachineGame {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
